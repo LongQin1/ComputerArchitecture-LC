f158396266935cb5d6b4ee9791e5737349b12cac
shinlong <vincentqin2016@gmail.com>
2018-11-17 16:50:13 +0100

    add some instructions

--- src/task1/IsaSim.java
+++ src/task1/IsaSim.java
@@ -33,12 +33,113 @@ public class IsaSim {
 			int opcode = instr & 0x7f;
 			int rd = (instr >> 7) & 0x01f;
 			int rs1 = (instr >> 15) & 0x01f;
-			int imm = (instr >> 20);
+			int imm110 = (instr >> 20);
+			int imm1210 = (instr>> 25 );
+			int imm115 = (instr>> 25 );
+			int imm40 = (instr>>7)& 0x01f;
+			int imm41 = (instr>>7) &0x01f;
+			int rs2 = (instr>> 20) &  0x01f;
+			int funct3=(instr>> 12) & 0x06f;
+			int imm3112 = (instr>>12);
+			int imm2010 = (instr>>12);
 
 			switch (opcode) {
-
+			//U-type
+			//LUI
+			case 0x37:
+				reg[rd]=imm3112 << 12;
+				break;
+			// AUIPC
+			case 0x17:
+				reg[rd]=(imm3112 <<12) + pc;
+				break;
+			//J-type
+			// I-type
+			//ANDI
 			case 0x13:
-				reg[rd] = reg[rs1] + imm;
+				switch(funct3){
+				case 0x0:	
+					reg[rd] = reg[rs1] + imm110;
+					break;
+				case 0x2:
+					if(reg[rs1] <imm110) {
+						reg[rd]=1;		
+					}else {
+						reg[rd]=0;
+					}
+					break;
+					// SLTIU
+				case 0x3:
+					if((imm110 >> 11) ==1){
+						if(reg[rs1]< 0xFFFFF000 + imm110){ 
+							//not signed extended imm12, and what does it mean to do an unsigned comparison.
+							reg[rd]=1;}
+						else {reg[rd]=0;}
+					}
+					else {
+						//Note, SLTIU rd, rs1, 1 sets rd to 1 if rs1 equals zero, otherwise sets rd to 0 (assembler pseudo-op SEQZ rd, rs).   ?????
+						if(reg[rs1]==imm110) {
+							reg[rd]=1;}
+						else {
+							reg[rd]=0;
+							}
+					}
+					break;
+				//XORI
+				case 0x4:
+					if((imm110 >> 11 )==1){
+						if((reg[rs1] >> 4) ==1) {
+							reg[rd] = (0xFFFFF000 + imm110 )^(0x7FFFFFE0 + reg[rs1]);
+						}else {
+							reg[rd] = (0xFFFFF000 + imm110 )^(reg[rs1]);
+						}
+					}else {
+						if((reg[rs1] >> 4) ==1) {
+						reg[rd] = (0xFFFFF000 + imm110 )^(0x7FFFFFE0 + reg[rs1]);
+						}else {
+						reg[rd] = (0xFFFFF000 + imm110 )^(reg[rs1]);
+						}
+					}
+					break;
+				
+				//XORI
+				case 0x6:
+					if((imm110 >> 11 )==1){
+						if((reg[rs1] >> 4) ==1) {
+							reg[rd] = (0xFFFFF000 + imm110 )|(0x7FFFFFE0 + reg[rs1]);
+						}else {
+							reg[rd] = (0xFFFFF000 + imm110 )|(reg[rs1]);
+						}
+					}else {
+						if((reg[rs1] >> 4) ==1) {
+							reg[rd] = (0xFFFFF000 + imm110 )|(0x7FFFFFE0 + reg[rs1]);
+						}else {
+							reg[rd] = (0xFFFFF000 + imm110 )|(reg[rs1]);
+						}	
+					}
+					break;
+				case 0x07:
+					if((imm110 >> 11 )==1){
+						if((reg[rs1] >> 4) ==1) {
+							reg[rd] = (0xFFFFF000 + imm110 )&(0x7FFFFFE0 + reg[rs1]);
+						}else {
+							reg[rd] = (0xFFFFF000 + imm110 )&(reg[rs1]);
+						}
+					}else {
+						if((reg[rs1] >> 4) ==1) {
+							reg[rd] = (0xFFFFF000 + imm110 )&(0x7FFFFFE0 + reg[rs1]);
+						}else {
+							reg[rd] = (0xFFFFF000 + imm110 )&(reg[rs1]);
+						}	
+					}
+					break;
+				
+				case 0x01:
+					reg[rd]= reg[rs1] << rs2;
+					break;
+				case 0x05:
+					 
+				}
 				break;
 			default:
 				System.out.println("Opcode " + opcode + " not yet implemented");
