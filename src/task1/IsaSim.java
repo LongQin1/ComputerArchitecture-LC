217b3ebed140fec7dedfbb7e63df8340cbea6f0b
shinlong <vincentqin2016@gmail.com>
2018-11-19 17:00:46 +0100

    new update for instructions

--- src/task1/IsaSim.java
+++ src/task1/IsaSim.java
@@ -54,7 +54,7 @@ public class IsaSim {
 			case 0x17:
 				reg[rd]=(imm3112 <<12) + pc;
 				break;
-			//J-type
+			//J-type JAL in our case (jump and link)
 			// I-type
 			//ANDI
 			case 0x13:
@@ -73,13 +73,13 @@ public class IsaSim {
 				case 0x3:
 					if((imm110 >> 11) ==1){
 						if(reg[rs1]< 0xFFFFF000 + imm110){ 
-							//not signed extended imm12, and what does it mean to do an unsigned comparison.
+							//comparison with sign extended
 							reg[rd]=1;}
 						else {reg[rd]=0;}
 					}
 					else{
 						//Note, SLTIU rd, rs1, 1 sets rd to 1 if rs1 equals zero, otherwise sets rd to 0 (assembler pseudo-op SEQZ rd, rs).   ?????
-						if(reg[rs1]==imm110) {
+						if(reg[rs1] < imm110) {
 							reg[rd]=1;}
 						else {
 							reg[rd]=0;
@@ -89,50 +89,48 @@ public class IsaSim {
 				//XORI
 				case 0x4:
 					if((imm110 >> 11) ==1){
-						if((reg[rs1] >> 4) ==1) {
-							reg[rd] = (0xFFFFF000 + imm110 )^(0x7FFFFFE0 + reg[rs1]);
-						}else {
+						//if((reg[rs1] >> 4) ==1) {
 							reg[rd] = (0xFFFFF000 + imm110 )^(reg[rs1]);
-						}
-					}else {
-						if((reg[rs1] >> 4) ==1) {
-						reg[rd] = (0xFFFFF000 + imm110 )^(0x7FFFFFE0 + reg[rs1]);
 						}else {
-						reg[rd] = (0xFFFFF000 + imm110 )^(reg[rs1]);
+							reg[rd] = (imm110 )^(reg[rs1]);
 						}
-					}
 					break;
-				
+					//}else {
+						//if((reg[rs1] >> 4) ==1) {
+					//	reg[rd] = (0xFFFFF000 + imm110 )^(0x7FFFFFE0 + reg[rs1]);
+					//	}else {
+					//	reg[rd] = (0xFFFFF000 + imm110 )^(reg[rs1]);
+					//}
 				//ORI
 				case 0x6:
 					if((imm110 >> 11 )==1){
-						if((reg[rs1] >> 4) ==1) {
-							reg[rd] = (0xFFFFF000 + imm110 )|(0x7FFFFFE0 + reg[rs1]);
-						}else {
+						//if((reg[rs1] >> 4) ==1) {
 							reg[rd] = (0xFFFFF000 + imm110 )|(reg[rs1]);
+						}else {
+							reg[rd] = (imm110 )|(reg[rs1]);
 						}
-					}else {
-						if((reg[rs1] >> 4) ==1) {
-							reg[rd] = (0xFFFFF000 + imm110 )|(0x7FFFFFE0 + reg[rs1]);
-						}else {
-							reg[rd] = (0xFFFFF000 + imm110 )|(reg[rs1]);
-						}	
-					}
 					break;
+				//	}else {
+				//		if((reg[rs1] >> 4) ==1) {
+				//			reg[rd] = (0xFFFFF000 + imm110 )|(0x7FFFFFE0 + reg[rs1]);
+				//		}else {
+				//			reg[rd] = (0xFFFFF000 + imm110 )|(reg[rs1]);
+				//		}	
+				//	}
 				case 0x07:
 					if((imm110 >> 11 )==1){
-						if((reg[rs1] >> 4) ==1) {
-							reg[rd] = (0xFFFFF000 + imm110 )&(0x7FFFFFE0 + reg[rs1]);
-						}else {
+				//		if((reg[rs1] >> 4) ==1) {
 							reg[rd] = (0xFFFFF000 + imm110 )&(reg[rs1]);
+						}else {
+							reg[rd] = (imm110 )&(reg[rs1]);
 						}
-					}else {
-						if((reg[rs1] >> 4) ==1) {
-							reg[rd] = (0xFFFFF000 + imm110 )&(0x7FFFFFE0 + reg[rs1]);
-						}else {
-							reg[rd] = (0xFFFFF000 + imm110 )&(reg[rs1]);
-						}	
-					}
+				//	}else {
+				//		if((reg[rs1] >> 4) ==1) {
+				//			reg[rd] = (0xFFFFF000 + imm110 )&(0x7FFFFFE0 + reg[rs1]);
+				//		}else {
+				//			reg[rd] = (0xFFFFF000 + imm110 )&(reg[rs1]);
+				//		}	
+				//	}
 					break;
 				// SLLI
 				case 0x01:
@@ -142,14 +140,9 @@ public class IsaSim {
 				case 0x05:
 					switch(imm115){
 					case 0x00:
-						reg[rd]= reg[rs1] >> rs2;
+						reg[rd]= reg[rs1] >>> rs2;
 					case 0x20:
-						if ((imm40 >>4)==1) {
-							reg[rd]= (reg[rs1] >> rs2)+(80000000);
-						}else {
-							reg[rd]= reg[rs1] >>rs2;
-						}
-						break;
+						reg[rd]= reg[rs1] >> rs2;
 					}
 					break;
 					 
@@ -179,10 +172,10 @@ public class IsaSim {
 							break;
 						case 0x20:
 							if((reg[rs1] >> 31) ==1){
-								if((reg[rs2] >>4) ==1) {
-									reg[rd] =reg[rs1]>>(reg[rs2]& 0x1F);
+								if((reg[rs1] >>(reg[rs1]>>0x1f)>>31)==1) {
+									reg[rd] =(reg[rs1]>>rs2) +(00000000);
 								}else {
-									reg[rd] = reg[]
+									reg[rd] = (reg[rs1]>>rs2);
 								}
 							}else {
 								
@@ -201,11 +194,33 @@ public class IsaSim {
 						break;
 					case 0x04://XOR case
 						reg[rd]=reg[rs1]^reg[rs2];
-						
 						break;
-
+					case 0x06://or case
+						reg[rd]=reg[rs1]|reg[rs2];
+						break;
+					case 0x07:
+						reg[rd]=reg[rs1]&reg[rs2];
+						break;
+	
 				}
-				
+			case 0x73: // ECALL https://github.com/kvakil/venus/wiki/Environmental-Calls 
+				if(reg[0x1010]==1) {
+					System.out.println(reg[0x1011]);
+				}else if(reg[0x1010] == 4){
+					System.out.println(reg[0x1011]);
+				}else if (reg[0x1010] == 9){ // using pointer couldn't do it rightn ow
+					
+				}else if( reg[0x1010] == 10) { //ends the program
+					System.exit(1);
+				}else if(reg[0x1010] == 11) {//prints ASCII character in a1
+					System.out.println(reg[0x1011]);
+				}else if(reg[0x1010] == 17){ // we don't this end the program with return code ,try it after running program
+					System.exit(reg[0x1011]);
+				}else {
+					System.out.println("please enter the right a0");
+				}
+				break;
+
 			default:
 				System.out.println("Opcode " + opcode + " not yet implemented");
 				break;
